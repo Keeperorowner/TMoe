f753074496072e1bb6e5dc5ad9da61dc59c2b025
Keeperorowner <qingfong0731@outlook.com>
2024-10-12 12:31:13 +0800

    更新 FakePremium.kt

--- app/src/main/java/cc/ioctl/tmoe/hook/func/FakePremium.kt
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/FakePremium.kt
@@ -1,8 +1,10 @@
 package cc.ioctl.tmoe.hook.func
 
+import cc.ioctl.tmoe.base.annotation.FunctionHookEntry
 import cc.ioctl.tmoe.hook.base.CommonDynamicHook
 import com.github.kyuubiran.ezxhelper.utils.*
 
+@FunctionHookEntry
 object FakePremium : CommonDynamicHook() {
     override fun initOnce(): Boolean = tryOrFalse {
         findMethod(loadClass("org.telegram.messenger.UserConfig")){
